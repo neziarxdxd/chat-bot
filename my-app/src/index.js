ac361e22a6024b954323d64e92fa4fc49d51ea5d
raizengxd <rickdavidsangalang@gmail.com>
2020-09-20 10:19:42 +0800

    add

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,16 @@
 module.exports = async function App(context) {
+  await context.sendButtonTemplate('What do you want to do next?', [
+    {
+      type: 'web_url',
+      url: 'https://petersapparel.parseapp.com',
+      title: 'Show Website',
+    },
+    {
+      type: 'postback',
+      title: 'Start Chatting',
+      payload: 'USER_DEFINED_PAYLOAD',
+    },
+  ]);
   var text = context.event.text;
   if (context.event.isText){
     await context.sendText('TEXT testing'+text);
