5e97f0a9cc795db7d8113e4f42d05b2081b527b8
raizengxd <rickdavidsangalang@gmail.com>
2020-09-21 23:40:21 +0800

    add done

--- test-api/app.js
+++ test-api/app.js
@@ -1,11 +1,33 @@
 const fetch = require('node-fetch');
 
-let url = "";
+function getSummary(pageSearchID){
+    console.log("test");
+    let settings = { method: "Get" };
+    
+    let url = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${pageSearchID}`
+    fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            var summarypage =json["query"]["pages"][`${pageSearchID}`]["extract"];            
+            console.log(summarypage)     
+            
+    });
+
+}
+
 
 let settings = { method: "Get" };
-
+let searchQuery = "Java"
+let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
 fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
-        // do something with JSON
+        var searches =json["query"]["search"];
+        searches.map((search,index ) => {
+            console.log(index,search.title)
+            return search
     });
+    console.log(searches[17]["pageid"])  
+    getSummary(searches[19]["pageid"]);     
+        
+});
