bb68417db4476184c2005b7359570a94dc85afd2
zenraiching <rickdavidsangalang@gmail.com>
2020-12-18 13:00:06 +0800

    add

--- DECEMBER/zaki_app/src/index.js
+++ DECEMBER/zaki_app/src/index.js
@@ -1,18 +1,3 @@
-const { router, text } = require('bottender/router');
-
-async function SayHi(context) {
-  await context.sendText('Hi!');
-}
-
-async function SayHello(context) {
-  await context.sendText('Hello!');
-}
-
-async function App() {
-  return router([
-    // return the `SayHi` action when receiving "hi" text messages
-    text('hi', SayHi),
-    // return the `SayHello` action when receiving "hello" text messages
-    text('hello', SayHello),
-  ]);
-}
+module.exports = async function App(context) {
+  await context.sendText('Welcome to Bottender');
+};
