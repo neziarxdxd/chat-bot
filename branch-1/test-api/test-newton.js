c679cf09d2516247a71825714c03fb9ec98aa0a7
zenraiching <rickdavidsangalang@gmail.com>
2020-12-19 23:00:18 +0800

    succesful factored

--- branch-1/test-api/test-newton.js
+++ branch-1/test-api/test-newton.js
@@ -1,2 +1,11 @@
 const fetch = require('node-fetch');
-
+var operation = "factor";
+var expression = "x^2-25";
+var encodedUrl = encodeURI(expression);
+/**
+fetch(`https://newton.now.sh/api/v2/${operation}/${expression}`)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json.expression)
+        console.log(json.result)});
+**/
