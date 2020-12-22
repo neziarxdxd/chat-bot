14d72999be71561f2eadbe6cab836250f809f00c
zenraiching <rickdavidsangalang@gmail.com>
2020-12-23 02:36:31 +0800

    faster bot

--- DECEMBER/zaki_app/src/index.js
+++ DECEMBER/zaki_app/src/index.js
@@ -1,22 +1,34 @@
-const { router, route } = require('bottender/router');
+const { router, route,text } = require('bottender/router');
 const fetch = require('node-fetch');
 
-
-
-
-
-
-async function test(context){
-  await context.sendText("TEST");
-}
-
-async function SendHi(context) {
-  await context.sendText('Hi!');
-}
-module.exports = async function App(context) {
-  return router([    
-    text('hi', SendHi),  
-    text(/^\/w/,test(context)),
+async function factor(context){
+  var operation = "factor";
+  var expression = "x^2-25";//context.event.text;
+  var encodedUrl = encodeURIComponent(expression);
+  var equation =`https://newton.now.sh/api/v2/${operation}/${encodedUrl}`;
+  fetch(equation)
+  .then(res => res.json())
+  .then(json =>{
     
-  ]);
+    context.sendText(json.result+" : "+context.event.text)});  
+}
+
+async function getBlock(context) {
+  let jsonBlocks;
+  var operation = "factor";
+  var expression = context.event.text;
+  var encodedUrl = encodeURIComponent(expression);
+
+  try {
+    var response = await fetch(`https://newton.now.sh/api/v2/${operation}/${encodedUrl}`);
+    jsonBlocks = await response.json();
+    context.sendText(jsonBlocks.result)
+  } catch (e) {
+    // handle error
+    console.error(e)
+  }
+}
+
+module.exports = async function App(context) {
+  return getBlock(context);
 };
--- branch-1/test-api/test-newton.js
+++ branch-1/test-api/test-newton.js
@@ -1,11 +1,20 @@
 const fetch = require('node-fetch');
-var operation = "zeroes";
-var expression = "x^2-25";
-var encodedUrl = encodeURIComponent(expression);
 
 
-fetch(`https://newton.now.sh/api/v2/${operation}/${encodedUrl}`)
-    .then(res => res.json())
-    .then(json => {
-        console.log(json.result)
-        });
+
+async function getBlock() {
+    let jsonBlocks;
+    var operation = "zeroes";
+    var expression = "x^2-25";
+    var encodedUrl = encodeURIComponent(expression);
+
+    try {
+      var response = await fetch(`https://newton.now.sh/api/v2/${operation}/${encodedUrl}`);
+      jsonBlocks = await response.text();
+      return await jsonBlocks
+    } catch (e) {
+      // handle error
+      console.error(e)
+    }
+  }
+
