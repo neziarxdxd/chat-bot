a4214111c86086a68492d672dc849e616703655c
zenraiching <rickdavidsangalang@gmail.com>
2020-12-24 20:30:54 +0800

    add comments

--- DECEMBER/zaki_app/src/index.js
+++ DECEMBER/zaki_app/src/index.js
@@ -1,54 +1,40 @@
 const { router, route,text } = require('bottender/router');
 const fetch = require('node-fetch');
 
-//delete immediately
+async function doSolveThis(context){
 
-async function testProd(context){
-  let jsonBlocks;
-  var equat = context.event.text;
-  var operation="factor";
-  var encodedUrl = encodeURIComponent(equat);
-  try {
-    var response = await fetch(`https://newton.now.sh/api/v2/${operation}/${encodedUrl}`);
-    jsonBlocks = await response.json();
-    // context.sendText(jsonBlocks.result)
-    console.log("test: "+equat)
-  }catch (e) {
-    // handle error
-    console.error(e)
-    
-  }
-}
-
-async function testMore(context){
+  //get the text
   var parseString = context.event.text; 
+
+  // this will split the string by space 
   var splittedString = parseString.split(" ")
+  
+  // removes the forward slash and get the function 
   var command = splittedString[0].substring(1,)
+  
+  // this get the whole equation 
   var equation = splittedString[1]
+  
+  // send the Command and Equation 
   await context.sendText("command: "+command)
   await context.sendText("equation: "+equation)
-
-  let jsonBlocks;  
   
+  let jsonBlocks;  
   var encodedUrl = encodeURIComponent(equation);
+  
   try {
     var response = await fetch(`https://newton.now.sh/api/v2/${command}/${encodedUrl}`);
     jsonBlocks = await response.json();
     // context.sendText(jsonBlocks.result)
     await context.sendText(`result: ${jsonBlocks.result}`)
-  }catch (e) {
-    // handle error
-    console.error(e)
-    
   }
-
-
-
+  
+  catch (e) {    
+    console.error(e)    
+  }
 }
 
-async function hi(context){
-  await context.sendText("TESTING")
-}
+
 
 
 
@@ -56,17 +42,17 @@ module.exports = async function App(context) {
   return router(
     [
      
-      text(/^\/simplify[]?\s+/,testMore),  
-      text(/^\/factor[]?\s+/,testMore),  
-      text(/^\/derive[]?\s+/,testMore),  
-      text(/^\/integrate[]?\s+/,testMore),  
-      text(/^\/zeroes[]?\s+/,testMore),  
-      text(/^\/tangent[]?\s+/,testMore),  
-      text(/^\/area[]?\s+/,testMore),  
-      text(/^\/cos[]?\s+/,testMore),
-      text(/^\/sin[]?\s+/,testMore), 
-      text(/^\/hello[]?\s+/,hi), 
-      text("hello",hi) 
+      text(/^\/simplify[]?\s+/,doSolveThis),  
+      text(/^\/factor[]?\s+/,doSolveThis),  
+      text(/^\/derive[]?\s+/,doSolveThis),  
+      text(/^\/integrate[]?\s+/,doSolveThis),  
+      text(/^\/zeroes[]?\s+/,doSolveThis),  
+      text(/^\/tangent[]?\s+/,doSolveThis),  
+      text(/^\/area[]?\s+/,doSolveThis),  
+      text(/^\/cos[]?\s+/,doSolveThis),
+      text(/^\/sin[]?\s+/,doSolveThis), 
+      
+      
          
       
     ]
