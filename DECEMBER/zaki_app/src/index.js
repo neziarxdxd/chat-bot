0a9d6b8ee088214825e47949f455ff7670c363db
zenraiching <rickdavidsangalang@gmail.com>
2020-12-25 10:05:43 +0800

    fix bug spaces

--- DECEMBER/zaki_app/src/index.js
+++ DECEMBER/zaki_app/src/index.js
@@ -5,32 +5,42 @@ async function doSolveThis(context){
   //get the text
   var parseString = context.event.text;
   // this will split the string by space 
-  var splittedString = parseString.split(" ")  
+  var splittedString = parseString.split(" ");  
   // removes the forward slash and get the function 
-  var command = splittedString[0].substring(1,)  
+  var command = splittedString[0].substring(1,);
   // this get the whole equation 
-  var equation = splittedString[1]  
+  var eq = splittedString.slice(1, splittedString.length );  
+  var equation= eq.join(" ");
   // send the Command and Equation 
   await context.sendText("command: "+command)
   await context.sendText("equation: "+equation)
   
   let jsonBlocks;  
+  // transform the equation to URI
   var encodedUrl = encodeURIComponent(equation);
   
   try {
+    // fetch 
     var response = await fetch(`https://newton.now.sh/api/v2/${command}/${encodedUrl}`);
     jsonBlocks = await response.json();
-    // context.sendText(jsonBlocks.result)
+    
     await context.sendText(`result: ${jsonBlocks.result}`)
   }
   
   catch (e) {    
     console.error(e)    
   }
+ 
+}
+
+async function sendHelp(context){
+  await context.sendText("ANG GANDA MO!!!")
+  //await context.sendText("Hi choose appropriate commands")
 }
 
 
 module.exports = async function App(context) {
+  // list of mathematics commnands
   return router(
     [     
       text(/^\/simplify[]?\s+/,doSolveThis),  
@@ -42,7 +52,8 @@ module.exports = async function App(context) {
       text(/^\/area[]?\s+/,doSolveThis),  
       text(/^\/cos[]?\s+/,doSolveThis),
       text(/^\/sin[]?\s+/,doSolveThis),
-      text('*',sendHelp)
+      
+      route('*',sendHelp)
     ]
 
   );
