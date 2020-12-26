8806c6f07f53627f0ccd62c5582fc0d7c9c60543
zenraiching <rickdavidsangalang@gmail.com>
2020-12-26 19:33:58 +0800

    ad

--- DECEMBER/zaki_app/src/index.js
+++ DECEMBER/zaki_app/src/index.js
@@ -2,31 +2,34 @@ const { router, route,text } = require('bottender/router');
 const fetch = require('node-fetch');
 
 async function doSolveThis(context){
+
   //get the text
-  var parseString = context.event.text;
+  var parseString = context.event.text
+
   // this will split the string by space 
-  var splittedString = parseString.split(" ");  
+  var splittedString = parseString.split(" ")
+
   // removes the forward slash and get the function 
-  var command = splittedString[0].substring(1,);
+  var command = splittedString[0].substring(1,)
+
   // this get the whole equation 
-  var eq = splittedString.slice(1, splittedString.length );  
+  var eq = splittedString.slice(1, splittedString.length )  
   var equation= eq.join(" ");
+
   // send the Command and Equation 
   await context.sendText("command: "+command)
   await context.sendText("equation: "+equation)
   
-  let jsonBlocks;  
+    
   // transform the equation to URI
-  var encodedUrl = encodeURIComponent(equation);
+  var encodedUrl = encodeURIComponent(equation)
   
   try {
     // fetch 
-    var response = await fetch(`https://newton.now.sh/api/v2/${command}/${encodedUrl}`);
-    jsonBlocks = await response.json();
-    
+    var response = await fetch(`https://newton.now.sh/api/v2/${command}/${encodedUrl}`)
+    var jsonBlocks = await response.json()
     await context.sendText(`result: ${jsonBlocks.result}`)
-  }
-  
+  }  
   catch (e) {    
     console.error(e)    
   }
@@ -52,6 +55,7 @@ module.exports = async function App(context) {
       text(/^\/area[]?\s+/,doSolveThis),  
       text(/^\/cos[]?\s+/,doSolveThis),
       text(/^\/sin[]?\s+/,doSolveThis),
+      text(/^\/log[]?\s+/,doSolveThis),
       
       route('*',sendHelp)
     ]
